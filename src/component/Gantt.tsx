bcda18fd61bca78c09d7a7399dd5b0f10a1cb2b4
Eugene Smoley <esmoley@gmail.com>
2024-04-17 19:23:18 +0300

    refactor: inner use tasks wrap impl

--- src/component/Gantt.tsx
+++ src/component/Gantt.tsx
@@ -2,17 +2,32 @@ import React from 'react'
 import { getDayOfWeek, getDaysInMonth, monthDiff } from '../util/ganttUtils'
 import { Locale } from '../util/types'
 import './styles.css'
+type TaskDate = Date | number
 type Task = {
   id: string
   name: string
-  start: Date
-  end: Date
+  start: TaskDate
+  end: TaskDate
   dependencies?: string[]
 }
+
 type GanntProps = {
   tasks: Task[]
   locale?: Locale
   theme?: 'light' | 'dark'
+  //scale?: 'month'
+}
+type TaskGraph = {
+  index: number
+  task: Task
+  start: number
+  end: number
+
+  dependencies: TaskGraph[]
+
+  x?: number
+  y?: number
+  width?: number
 }
 const cellWidth = 30
 const cellHeight = 40
@@ -22,20 +37,37 @@ const dayMs = new Date(0).setDate(2)
 export const Gantt = ({ tasks, locale, theme }: GanntProps) => {
   if (!theme) theme = 'light'
   if (!locale) locale = 'en'
-  const startMonth = new Date(
-    new Date(
-      tasks.reduce((acc, task) => {
-        if (task.start.getTime() < acc.start.getTime()) return task
-        return acc
-      }, tasks[0]).start,
-    ).setDate(1),
+
+  //if (!scale) scale = 'month'
+  //const scale = 'month'
+  const taskGraphMap: Map<string, TaskGraph> = tasks.reduce((acc, task, index) => {
+    return acc.set(task.id, {
+      index,
+      task: task,
+      start: task.start instanceof Date ? task.start.getTime() : task.start,
+      end: task.end instanceof Date ? task.end.getTime() : task.end,
+      dependencies: [],
+    })
+  }, new Map<string, TaskGraph>())
+  const taskGraphArr: TaskGraph[] = Array.from(taskGraphMap.values())
+
+  //udpate deps
+  taskGraphMap.forEach((t) =>
+    t.task.dependencies?.forEach((tDepId) => taskGraphMap.has(tDepId) && t.dependencies.push(taskGraphMap.get(tDepId))),
   )
-  const endMonth = new Date(
-    tasks.reduce((acc, task) => {
-      if (task.end.getTime() > acc.end.getTime()) return task
-      return acc
-    }, tasks[0]).end,
+
+  const lowestTaskGraphStart: TaskGraph = taskGraphArr.reduce(
+    (acc, task) => (task.start < acc.start ? task : acc),
+    taskGraphArr[0],
   )
+  const highestTaskGraphEnd: TaskGraph = taskGraphArr.reduce(
+    (acc, task) => (task.end > acc.end ? task : acc),
+    taskGraphArr[0],
+  )
+
+  const startMonth = new Date(new Date(lowestTaskGraphStart.start).setDate(1))
+  const endMonth = new Date(highestTaskGraphEnd.end)
+
   const monthsAmount = monthDiff(startMonth, endMonth) + 1
   const DaysRow = ({ y }: { y: number }) => {
     const month = new Date(startMonth)
@@ -83,23 +115,20 @@ export const Gantt = ({ tasks, locale, theme }: GanntProps) => {
     return <g>{res}</g>
   }
   const Dependencies = ({ y }: { y: number }) => {
-    return tasks.map((task, taskIndex) => {
-      if (!task.dependencies) return null
-      return tasks.map((depTask, depTaskIndex) => {
-        if (!task.dependencies.includes(depTask.id)) return null
-        const taskStartX = ((task.start.getTime() - startMonth.getTime()) / dayMs) * cellWidth
-        const taskY = y + taskIndex * cellHeight + cellHeight / 2
+    return taskGraphArr.map((t) => {
+      if (t.dependencies.length === 0) return null
+      return t.dependencies.map((tD) => {
+        const taskStartX = ((t.start - startMonth.getTime()) / dayMs) * cellWidth
+        const taskY = y + t.index * cellHeight + cellHeight / 2
 
-        const depEndX =
-          ((depTask.start.getTime() - startMonth.getTime() + depTask.end.getTime() - depTask.start.getTime()) / dayMs) *
-          cellWidth
-        const depY = y + depTaskIndex * cellHeight + cellHeight / 2
-        const isTaskHigher = taskIndex < depTaskIndex
+        const depEndX = ((tD.start - startMonth.getTime() + tD.end - tD.start) / dayMs) * cellWidth
+        const depY = y + tD.index * cellHeight + cellHeight / 2
+        const isTaskHigher = t.index < tD.index
         const h2 = taskStartX - depEndX - 20
         const v1Text = `v ${isTaskHigher ? '-' : ''}${cellHeight / 2}`
         const h2Text = `h ${taskStartX - depEndX - 20}`
         return (
-          <g key={task.id}>
+          <g key={t.task.id}>
             <path
               stroke='grey'
               fill='none'
@@ -107,7 +136,7 @@ export const Gantt = ({ tasks, locale, theme }: GanntProps) => {
               d={`M ${depEndX} ${depY} 
 							h 10
 							${h2 > 0 ? h2Text + v1Text : v1Text + h2Text}
-							v ${cellHeight * (taskIndex - depTaskIndex) + ((isTaskHigher ? 1 : -1) * cellHeight) / 2}
+							v ${cellHeight * (t.index - tD.index) + ((isTaskHigher ? 1 : -1) * cellHeight) / 2}
 							h 10 `}
             />
             <polygon
@@ -124,12 +153,12 @@ export const Gantt = ({ tasks, locale, theme }: GanntProps) => {
   const TaskRowsTimePeriods = ({ y }: { y: number }) => {
     return (
       <g>
-        {tasks.map((task, taskIndex) => {
-          const x = ((task.start.getTime() - startMonth.getTime()) / dayMs) * cellWidth
-          const width = ((task.end.getTime() - task.start.getTime()) / dayMs) * cellWidth
+        {taskGraphArr.map((t, taskIndex) => {
+          const x = ((t.start - startMonth.getTime()) / dayMs) * cellWidth
+          const width = ((t.end - t.start) / dayMs) * cellWidth
           return (
             <rect
-              key={`${task.id}`}
+              key={`${t.task.id}`}
               className='task-rect'
               x={x}
               y={y + taskIndex * cellHeight + 7}
@@ -173,39 +202,32 @@ export const Gantt = ({ tasks, locale, theme }: GanntProps) => {
     const res = []
     const month = new Date(startMonth)
     let curX = 0
+    const renderDay = (day: number, curX: number, currYear: number, currMonth: number) => {
+      const dayOfTheWeek = getDayOfWeek(currYear, currMonth - 1, day)
+      return (
+        <rect
+          key={`${currMonth}_${day}_${currYear}`}
+          x={curX}
+          y={y}
+          width={cellWidth}
+          height={cellHeight * tasks.length}
+          fill={dayOfTheWeek === 'S' ? '#f5f5f5' : '#fff'}
+        />
+      )
+    }
     for (let i = 0; i < monthsAmount; i++) {
       const currYear = month.getFullYear()
       const currMonth = month.getMonth() + 1
       const numDays = getDaysInMonth(currYear, currMonth)
       for (let j = i === 0 ? startMonth.getDate() - 1 : 0; j < numDays; j++) {
-        const dayOfTheWeek = getDayOfWeek(currYear, currMonth - 1, j)
-        if (dayOfTheWeek === 'S') {
-          res.push(
-            <rect
-              key={`${i}_${j}`}
-              x={curX}
-              y={y}
-              width={cellWidth}
-              height={cellHeight * tasks.length}
-              fill='#f5f5f5'
-            ></rect>,
-          )
-        } else {
-          res.push(
-            <rect
-              key={`${i}_${j}`}
-              x={curX}
-              y={y}
-              width={cellWidth}
-              height={cellHeight * tasks.length}
-              fill='#fff'
-            ></rect>,
-          )
-        }
+        res.push(renderDay(j, curX, currYear, currMonth))
         curX += cellWidth
       }
       month.setMonth(month.getMonth() + 1)
     }
+
+    res.push(renderDay(1, curX, month.getFullYear(), month.getMonth() + 1))
+    curX += cellWidth
     return <g>{res}</g>
   }
   const TimeGrid = () => {
@@ -246,12 +268,10 @@ export const Gantt = ({ tasks, locale, theme }: GanntProps) => {
   return (
     <div id='gantt-container' className={`react-gantt-accurate ${theme}`}>
       <div className='gantt-grid-container'>
-        <div id='gantt-grid-container__tasks' style={{ marginTop: `${cellHeight + 10}px` }}>
+        <div className='gantt-grid-container__tasks' style={{ marginTop: `${cellHeight + 10}px` }}>
           {tasks.map((task) => (
-            <div key={task.id}>
-              <div className='gantt-task-row' style={{ height: `${cellHeight}px` }}>
-                {task.name}
-              </div>
+            <div key={task.id} className='gantt-task-row' style={{ height: `${cellHeight}px` }}>
+              {task.name}
             </div>
           ))}
         </div>
