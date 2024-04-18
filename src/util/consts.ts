e1c24ed38556390d63ea10e8c06ccf2337bfcda8
Eugene Smoley <esmoley@gmail.com>
2024-04-18 18:38:06 +0300

    refactor: remove unused consts and funcs

--- src/util/consts.ts
+++ src/util/consts.ts
@@ -3,5 +3,3 @@ export const DaysOfWeekArrRu = ['Вс', 'Пн', 'Вт', 'Ср', 'Чт', 'Пт', 
 
 export const MonthsArrEn = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
 export const MonthsArrRu = ['Янв', 'Фев', 'Мар', 'Апр', 'Май', 'Июн', 'Июл', 'Авг', 'Сен', 'Окт', 'Ноя', 'Дек']
-
-export const DaySize = new Date(0, 0, 2, 0, 0, 0, 0).getTime() - new Date(0, 0, 1, 0, 0, 0, 0).getTime()
