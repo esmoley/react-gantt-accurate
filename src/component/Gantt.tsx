f2097befe28af73e3175d51cd3cf27d73f4c648d
Eugene Smoley <esmoley@gmail.com>
2024-07-18 14:57:06 +0300

    feat: adaptive tooltip

--- src/component/Gantt.tsx
+++ src/component/Gantt.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { getDayOfWeek } from '../util/ganttUtils'
 import { Locale } from '../util/types'
 import './styles.css'
@@ -44,23 +44,118 @@ const timePeriodHeight = cellHeight - 14
 
 type TaskTooltipProps = {
   id: string
-  left: number
   top: number
   show: boolean
+  hideTooltip?: () => void
   content: string | JSX.Element
+  taskX: number
+  taskWidth: number
 }
-const TaskTooltip = ({ left, top, show, content }: TaskTooltipProps) => {
+const screenPadding = 15
+const TaskTooltip = ({ top, show, content, taskX, taskWidth, hideTooltip }: TaskTooltipProps) => {
+  const tooltipChildRef = useRef<HTMLDivElement>(null)
+
+  const taskEndX = taskX + taskWidth
+  const tooltipRightToTaskX = screenPadding + taskEndX
+  const [left, setLeft] = useState<React.CSSProperties['left']>(tooltipRightToTaskX)
+  const [right, setRight] = useState<React.CSSProperties['right']>(null)
+  const [innerLeft, setInnerLeft] = useState<React.CSSProperties['left']>('50%')
+  const [innerRight, setInnerRight] = useState<React.CSSProperties['left']>(null)
+  const [topUpdated, setTopUpdated] = useState<React.CSSProperties['top']>(top)
+  const [step, setStep] = useState<'Initial' | 'UpdateOrientation' | 'SetTop' | 'SetBottom' | 'Done'>('Initial')
+  //const [lastUpdate, setLastUpdate] = useState<number>(Date.now())
+  const windowWidthScrolled = window.scrollX + window.innerWidth
+
+  useEffect(() => {
+    if (!show || !tooltipChildRef.current) {
+      setStep('Initial')
+      return
+    }
+    if (step != 'Initial') return
+
+    const rectChild = tooltipChildRef.current.getBoundingClientRect()
+    if (!rectChild) return
+    setLeft(tooltipRightToTaskX)
+    setRight(null)
+    setInnerLeft('50%')
+    setInnerRight(null)
+    setStep('UpdateOrientation')
+    setTopUpdated(top)
+  }, [show, tooltipRightToTaskX, top, step])
+
+  useEffect(() => {
+    if (!show || !tooltipChildRef.current || step !== 'UpdateOrientation') return
+    const rectChild = tooltipChildRef.current.getBoundingClientRect()
+    if (!rectChild) return
+    const tooltipActualWidth = rectChild.width
+    const tooltipRightX = tooltipRightToTaskX + tooltipActualWidth
+    if (tooltipActualWidth + screenPadding * 2 >= windowWidthScrolled) {
+      // is wider than screen
+      setLeft(0)
+      setRight(0)
+      setInnerLeft(0)
+      setInnerRight(0)
+    } else if (tooltipRightX > windowWidthScrolled) {
+      // is right oriented
+      setLeft('auto')
+      setRight(screenPadding)
+      setInnerLeft(null)
+      setInnerRight('50%')
+    }
+    setTopUpdated(top)
+    setStep('SetTop')
+  }, [step, show, tooltipRightToTaskX, top, windowWidthScrolled])
+
+  useEffect(() => {
+    if (!show || !tooltipChildRef.current || step !== 'SetTop') return
+    const rectChild = tooltipChildRef.current.getBoundingClientRect()
+    if (!rectChild) return
+    if (rectChild.top < 0) {
+      setTopUpdated(top + -rectChild.top + screenPadding)
+    }
+    setStep('Done')
+  }, [step, show, top])
+
+  // useEffect(() => {
+  //   const handleResize = () => {
+  //     if(step != "Done" || lastUpdate + 100 > Date.now()) return
+  //     setStep("Initial")
+  //   };
+  //   window.addEventListener('resize', handleResize);
+  //   return () => {
+  //     window.removeEventListener('resize', handleResize);
+  //   };
+  // }, [step, lastUpdate]);
+
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     if(step != "Done" || lastUpdate + 100 > Date.now()) return
+  //     setStep("Initial")
+  //   };
+  //   window.addEventListener('scroll', handleScroll);
+  //   return () => {
+  //     window.removeEventListener('scroll', handleScroll);
+  //   };
+  // }, [step, lastUpdate]);
+
   if (!show) return <></>
+
   return (
-    <div style={{ left, top, position: 'absolute' }}>
+    <div
+      style={{ left, right, top: topUpdated, position: 'absolute', opacity: step === 'Done' ? 1 : 0 }}
+      onMouseLeave={hideTooltip}
+    >
       <div
         style={{
           border: '1px solid black',
-          top: '50%',
-          transform: 'translateY(-50%)',
           color: 'black',
           background: '#fff',
+          transform: 'translateY(-50%)',
+          zIndex: 2,
+          left: innerLeft,
+          right: innerRight,
         }}
+        ref={tooltipChildRef}
       >
         {content}
       </div>
@@ -77,10 +172,11 @@ export const Gantt = ({
 }: GanntProps) => {
   const [taskTooltipProps, setTaskTooltipProps] = useState<TaskTooltipProps>({
     id: '',
-    left: 150,
     top: 150,
     show: false,
     content: null,
+    taskX: 0,
+    taskWidth: 0,
   })
   const cellMs =
     viewMode === 'hours'
@@ -426,6 +522,10 @@ export const Gantt = ({
       })
     })
   }
+  const hideTooltip = () => {
+    setTaskTooltipProps({ id: '', top: 0, show: false, content: null, taskX: 0, taskWidth: 0, hideTooltip })
+  }
+
   const TaskRowsTimePeriods = ({ y }: { y: number }) => {
     return (
       <g>
@@ -433,7 +533,6 @@ export const Gantt = ({
           const x = ((t.start - startDate.getTime()) / cellMs) * cellWidth
           const width = ((t.end - t.start) / cellMs) * cellWidth
           const curY = y + t.rowIndex * cellHeight + 7
-
           return (
             <rect
               key={`${t.task.id}`}
@@ -447,15 +546,18 @@ export const Gantt = ({
               style={{ cursor: t.task.tooltip ? 'pointer' : 'default' }}
               onMouseOverCapture={() => {
                 if (!t.task.tooltip || (taskTooltipProps.show && taskTooltipProps.id === t.task.id)) return
-                setTaskTooltipProps({
+                const upd = {
                   id: t.task.id,
-                  left: namesPanelWidth + x + 15 + width,
                   top: curY + timePeriodHeight / 2,
                   show: true,
                   content: t.task.tooltip,
-                })
+                  taskX: namesPanelWidth + x,
+                  taskWidth: width,
+                  hideTooltip,
+                }
+                setTaskTooltipProps(upd)
               }}
-              onMouseOutCapture={() => setTaskTooltipProps({ id: '', left: 0, top: 0, show: false, content: null })}
+              onMouseOutCapture={hideTooltip}
             />
           )
         })}
